bd31648a9f5cbbea206e4c84ba2331e563f7212d
BosEriko <bos.eriko@gmail.com>
2019-10-16 08:04:56 +0800

    [Hotfix] Add columns on the User model that are needed for logging in

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 2019_10_09_000354) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.string "name"
     t.string "nickname"
     t.string "image"
