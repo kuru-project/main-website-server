ef19ecdca1e80158e8b892076dbafeafef17e121
BosEriko <bos.eriko@gmail.com>
2020-01-04 13:07:38 +0800

    WIP - fix cors

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,10 @@ module Myapp
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*',
+          headers: :any,
+          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          methods: [:get, :post, :options, :delete, :put]
       end
     end
   end
