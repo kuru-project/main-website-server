e978136e037d2ee69bb3e0b7cd9b3be0bd3bf541
BosEriko <bos.eriko@gmail.com>
2020-01-12 18:17:04 +0800

    WIP - generated a model named Link

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_000354) do
+ActiveRecord::Schema.define(version: 2020_01_12_101032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "links", force: :cascade do |t|
+    t.string "url"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
