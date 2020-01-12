e990fb77a95bed81e9db8fb236871b1f03d2779b
BosEriko <bos.eriko@gmail.com>
2020-01-12 18:07:13 +0800

    WIP - configure graphiql

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  post "/data", to: "graphql#execute"
-  mount_devise_token_auth_for 'User', at: 'auth'
+  # Base Redirect
   root to: redirect("https://web.kuru-anime.com/")
+
+  # Authentication
+  mount_devise_token_auth_for 'User', at: 'auth'
+
+  # Data Management (GraphQL)
+  post "/data", to: "graphql#execute"
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/data"
+  end
 end
