d3a83ac15838fd6cf133dbc53d3fb65530f457f7
BosEriko <bos.eriko@gmail.com>
2020-01-13 18:27:00 +0800

    WIP - move the authentication to GraphqlController

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,5 +1,6 @@
 class GraphqlController < ApplicationController
-  skip_before_action :authenticate_user!
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :authenticate_user!
 
   def execute
     variables = ensure_hash(params[:variables])
