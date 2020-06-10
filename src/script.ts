84d31e80ed0d887c55825e5c755e004ad0a72f92
BosEriko <bos.eriko@gmail.com>
2020-06-10 18:08:32 +0800

    add a way to add user

--- src/script.ts
+++ src/script.ts
@@ -2,8 +2,15 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
-
+  const newUser = await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+    }
+  });
+  console.log(newUser);
 }
+
 main()
   .catch(e => {
     throw e
