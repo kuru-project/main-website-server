15b35dc951d4243703bb608d4a772efa2ecd648d
BosEriko <bos.eriko@gmail.com>
2020-06-10 17:52:32 +0800

    update prisma schema andd instantiate prisma client

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const Koa = require('koa');
 const app = new Koa();
+const prisma = new PrismaClient();
 
 app.use(async (ctx: any) => {
   ctx.body = 'Hello World';
