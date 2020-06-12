482dfe02d15a279d717287aba9744aa5d3bcc213
Bos Eriko Reyes <bos.eriko@gmail.com>
2020-06-12 10:18:18 +0800

    [Finishes #61] Setup koa-router
    # Story Title
    
    [Setup koa-router](https://github.com/kuru-project/main-website-server/issues/61)
    
    # Changes made
    
    - Revert the way to call Koa
    - Change the main file on package.json
    - Install Koa Router
    - Create 2 mock paths for future references
    
    # How does the solution address the problem
    
    This PR will add routing capabilities to the server.
    
    # Linked issues
    
    Resolves #61

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import Koa from 'koa';
+const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx: any) => {
+router.get('/hello', (ctx, next) => {
   ctx.body = 'Hello World';
 });
+
+router.get('/test', (ctx, next) => {
+  ctx.body = 'test';
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(process.env.PORT ?? 3000);
